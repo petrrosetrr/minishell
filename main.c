d73f18562e58972a419f3655e3c9395806efee92
Daisey Patrica <dpatrica@va-a2.kzn.21-school.ru>
2021-04-09 23:36:37 +0300

    tepmcup2

--- main.c
+++ main.c
@@ -5,17 +5,16 @@ int main(int argc, char **argv, char **envp)
 	char			str[9999];
 	int				len;
 	int				i;
+	int				up;
 //	int				bit;
-	int				down;
 	int				col;
 	struct termios	term;
 	struct winsize	win;
 
-	down = 0;
 	tcgetattr(0, &term);
 	term.c_lflag &= ~(ECHO); // -ехо
 	term.c_lflag &= ~(ICANON); // -посимвольно
-//	term.c_lflag &= ~(ISIG); // -сигналы отключить перед парсингом!
+//	term.c_lflag &= ~(ISIG); // -сигналы / отключить перед парсингом!
 	tcsetattr(0, TCSANOW, &term);
 //	term.c_cc[VMIN] = 200;
 	tgetent(0, "xterm-256color");
@@ -23,7 +22,7 @@ int main(int argc, char **argv, char **envp)
 //	FIXME траблы с esc
 	while (write(1, kek, 40))
 	{
-		i = 0;
+		i = 16;
 		tputs(save_cursor, 1, ft_putchar);
 		while ((len = read(0, str, 9999)))
 		{
@@ -32,90 +31,111 @@ int main(int argc, char **argv, char **envp)
 			str[len] = '\0';
 			if (!(ft_strncmp(str, "\x1b[A", 3)))
 			{
-				tputs(restore_cursor, 1, ft_putchar);
+				up = i / win.ws_col;
+				if (up)
+				{
+					i = i - (win.ws_col * (i / win.ws_col));
+					while (i--)
+					{
+						tputs(cursor_left, 1, ft_putchar);
+						tputs(delete_character, 1, ft_putchar);
+					}
+					tputs(delete_character, 1, ft_putchar);
+					tputs(cursor_up, 1, ft_putchar);
+					while (--up)
+					{
+						col = win.ws_col;
+						while (col--)
+							tputs(delete_character, 1, ft_putchar);
+						tputs(cursor_up, 1, ft_putchar);
+					}
+					i = 16;
+					while (i--)
+						tputs(cursor_right, 1, ft_putchar);
+					tputs(save_cursor, 1, ft_putchar);
+					i = win.ws_col;
+				}
+				else
+					tputs(restore_cursor, 1, ft_putchar);
 				write(1, "up", 2);
 				while (i--)
 					tputs(delete_character, 1, ft_putchar);
-				i = 2;
+				i = 16 + 2;
 			}
 			else if (!(ft_strncmp(str, "\x1b[B", 3)))
 			{
-				tputs(restore_cursor, 1, ft_putchar);
+				up = i / win.ws_col;
+				if (up)
+				{
+					i = i - (win.ws_col * (i / win.ws_col));
+					while (i--)
+					{
+						tputs(cursor_left, 1, ft_putchar);
+						tputs(delete_character, 1, ft_putchar);
+					}
+					tputs(delete_character, 1, ft_putchar);
+					tputs(cursor_up, 1, ft_putchar);
+					while (--up)
+					{
+						col = win.ws_col;
+						while (col--)
+							tputs(delete_character, 1, ft_putchar);
+						tputs(cursor_up, 1, ft_putchar);
+					}
+					i = 16;
+					while (i--)
+						tputs(cursor_right, 1, ft_putchar);
+					tputs(save_cursor, 1, ft_putchar);
+					i = win.ws_col;
+				}
+				else
+					tputs(restore_cursor, 1, ft_putchar);
 				write(1, "down", 4);
 				while (i--)
 					tputs(delete_character, 1, ft_putchar);
-				i = 4;
+				i = 16 + 4;
 			}
-//			else if (!(ft_strncmp(str, "\x1b[D", 3)))
-//				continue ;
-//			else if (!(ft_strncmp(str, "\x1b[C", 3)))
-//				continue ;
+			else if (!(ft_strncmp(str, "\x1b[D", 3)))
+				continue ;
+			else if (!(ft_strncmp(str, "\x1b[C", 3)))
+				continue ;
 			else if (!(ft_strncmp(str, "\t", 2)))
 				continue ;
-			else if (!(ft_strncmp(str, "\177", 2)) && i)
+//				tputs(delete_character, 1, ft_putchar);
+			else if (!(ft_strncmp(str, "\177", 2)) && i > 16)
 			{
-				if (down && ((i - (((win.ws_col - 1) * down) - 16)) == 0))
+				if (i / win.ws_col > 0 && !(i - (win.ws_col * (i / win.ws_col))))
 				{
-					col = win.ws_col - 2;
+					col = win.ws_col;
 					tputs(cursor_up, 1, ft_putchar);
 					while (col--)
 						tputs(cursor_right, 1, ft_putchar);
-					tputs(delete_character, 1, ft_putchar);
-					down--;
+					write(1, " ", 1);
+					tputs(cursor_left, 1, ft_putchar);
+					tputs(cursor_right, 1, ft_putchar);
 				}
 				else
 				{
+					tputs(delete_character, 1, ft_putchar);
 					tputs(cursor_left, 1, ft_putchar);
 					tputs(delete_character, 1, ft_putchar);
 				}
 				i--;
-/*				if (i == (win.ws_col - 16))
-				{
-					col = win.ws_col - 2;
-					tputs(cursor_up, 1, ft_putchar);
-					while (col--)
-						tputs(cursor_right, 1, ft_putchar);
-//					tputs(cursor_left, 1, ft_putchar);
-					tputs(delete_character, 1, ft_putchar);
-					i--;
-				}
-				else
-				{
-					tputs(cursor_left, 1, ft_putchar);
-					tputs(delete_character, 1, ft_putchar);
-					i--;
-				}*/
 			}
 			else if (ft_strncmp(str, "\177", 2))
 			{
-				if ((!down && i < (win.ws_col - 17)) ||\
-				(down && ((i - (((win.ws_col - 1) * down) - 17)) < win.ws_col)))
+				write(1, str, len);
+				i += len;
+				if (i / win.ws_col > 0 && !(i - (win.ws_col * (i / win.ws_col))))
 				{
-					write(1, str, len);
-					i += len;
+					write(1, " ", 1);
+					tputs(delete_character, 1, ft_putchar);
+					tputs(cursor_left, 1, ft_putchar);
+					tputs(delete_character, 1, ft_putchar);
 				}
-				if ((!down && i >= (win.ws_col - 17)) ||\
-				(down && ((i - (((win.ws_col - 1) * down) - 17)) >= win.ws_col)))
-				{
-					down++;
-					tputs(cursor_down, 1, ft_putchar);
-				}
-/*				else
-				{
-					down++;
-					tputs(cursor_down, 1, ft_putchar);
-					write(1, str, len);
-					i += len;
-				}*/
-//				if (!(ft_strncmp(str, "\t", 2)))
-//					i += 6;
 			}
 			if (!(ft_strncmp(str, "\n", 2)))
-			{
-//				tputs(cursor_down, 1, ft_putchar);
-//				printf("%d\n", win.ws_col);
 				break ;
-			}
 		}
 	}
 	return (0);
